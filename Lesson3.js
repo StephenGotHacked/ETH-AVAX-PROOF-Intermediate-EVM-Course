ac918eba01a636a6f4d12d99c86842d37251c347
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-05-13 03:29:10 +0800

    Update Lesson3.js

--- Lesson3.js
+++ Lesson3.js
@@ -1,16 +1,30 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-  const lockedAmount = hre.ethers.parseEther("0.001");
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Lesson3 = await ethers.getContractFactory("Lesson3");
   
-  await lock.waitForDeployment();
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const lesson3 = await Lesson3.deploy();
+  await lesson3.deployed();
+  console.log("Lesson3 deployed to:", lesson3.address);
+
+  const [sender, receiver] = await ethers.getSigners();
+
+  await lesson3.Deposit(receiver.address, 100);
+  console.log("Deposited 100 tokens to receiver");
+
+  await lesson3.Withdraw(sender.address, 50);
+  console.log("Withdrawn 50 tokens from sender");
+
+  await lesson3.Transfer(sender.address, receiver.address, 30);
+  console.log("Transferred 30 tokens from sender to receiver");
+
+  const receiverBalance = await lesson3.getBalance(receiver.address);
+  console.log("Receiver balance:", receiverBalance.toString());
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
