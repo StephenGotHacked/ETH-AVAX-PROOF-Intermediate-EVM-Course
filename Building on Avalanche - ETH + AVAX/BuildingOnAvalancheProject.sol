8509032f38c485538569316372eafdf9e247eb99
Stephen Condino <101857794+StephenGotHacked@users.noreply.github.com>
2024-05-30 02:25:24 +0800

    Update BuildingOnAvalancheProject.sol

--- Building on Avalanche - ETH + AVAX/BuildingOnAvalancheProject.sol
+++ Building on Avalanche - ETH + AVAX/BuildingOnAvalancheProject.sol
@@ -3,15 +3,6 @@ pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/*
-AGENDA!
-Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
-Transferring tokens: Players should be able to transfer their tokens to others.
-Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
-Checking token balance: Players should be able to check their token balance at any time.
-Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
-*/
-
 contract Assessment is ERC20 {
     
     address public OwnerAddress;
